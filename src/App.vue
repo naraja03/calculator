d712662fe10f0df1ec80af59fbec18a350bbc1fc
naraja03 <santhanraja3@gmail.com>
2021-12-27 18:43:28 +0530

    commit 2

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,29 @@
 <template>
-  <div class="main">
+  <div>
+    <h1>Calculator!</h1>
     <div class="calculator">
-      <div class="display">{{ current || 0 }}</div>
-      <div @click="del" class="button">DEL</div>
-      <div @click="clearAll" class="button">AC</div>
+      <div class="display">{{ current || "0" }}</div>
+      <div @click="clear" class="button">AC</div>
+      <div @click="two" class="button">+/-</div>
       <div @click="percent" class="button">%</div>
-      <div @click="divide" class="button operator">/</div>
+      <div @click="divide" class="operator">/</div>
       <div @click="append('7')" class="button">7</div>
       <div @click="append('8')" class="button">8</div>
       <div @click="append('9')" class="button">9</div>
-      <div @click="times" class="button operator">X</div>
+      <div @click="multiply" class="operator">*</div>
       <div @click="append('4')" class="button">4</div>
       <div @click="append('5')" class="button">5</div>
       <div @click="append('6')" class="button">6</div>
-      <div @click="minus" class="button operator">-</div>
+      <div @click="minus" class="operator">-</div>
       <div @click="append('1')" class="button">1</div>
       <div @click="append('2')" class="button">2</div>
       <div @click="append('3')" class="button">3</div>
-      <div @click="add" class="button operator">+</div>
-      <div @click="sign" class="button">(+/-)</div>
-      <div @click="append('0')" class="button">0</div>
-      <div @click="dot" class="button operator">.</div>
-      <div
-        @click="equal"
-        @keyup="enter"
-        v-show="buttonDisable"
-        class="button operator"
-      >
-        =
-      </div>
+      <div @click="add" class="operator">+</div>
+      <div @click="append(0)" class="button">0</div>
+      <div @click="del" class="button">DEL</div>
+
+      <div @click="dot" class="button">.</div>
+      <div @click="equal" class="operator">=</div>
     </div>
   </div>
 </template>
@@ -36,53 +31,48 @@
 <script>
 import { ref } from "vue";
 export default {
-  name: "App",
+  name: "Calculator",
   setup() {
-    let previous = ref(null);
     let current = ref("");
+    let previous = ref(null);
     let operator = ref(null);
-    let operatorClicked = ref(false);
-    let buttonDisable = ref(true);
-
+    let operatorClick = ref(false);
+    function clear() {
+      current.value = "";
+    }
+    function two() {
+      current.value =
+        current.value.charAt(0) === "-"
+          ? current.value.slice(1)
+          : `-${current.value}`;
+    }
+    function percent() {
+      current.value = `${current.value / 100}`;
+    }
     function append(number) {
-      if (operatorClicked.value) {
+      if (operatorClick.value) {
         current.value = "";
-        operatorClicked.value = false;
-        console.log("value 1", current.value);
+        operatorClick.value = false;
       }
       current.value = `${current.value}${number}`;
-      buttonDisable.value = true;
-      console.log("value 1", current.value);
+    }
+    function dot() {
+      if (current.value.indexOf(".") === -1) {
+        `${append(".")}`;
+      }
     }
     function del() {
       current.value = current.value.slice(0, -1);
     }
-    function percent() {
-      current.value = `${parseFloat(current.value) / 100}`;
-    }
-    function sign() {
-      current.value =
-        current.value.charAt(0) === "-"
-          ? `${current.value.slice(1)}`
-          : `-${current.value}`;
-    }
-    function clearAll() {
-      current.value = "";
-    }
-    function dot() {
-      if (current.value.indexOf(".") === -1) {
-        append(".");
-      }
-    }
     function setPrevious() {
       previous.value = current.value;
-      operatorClicked.value = true;
+      operatorClick.value = true;
     }
     function divide() {
       operator.value = (a, b) => a / b;
       setPrevious();
     }
-    function times() {
+    function multiply() {
       operator.value = (a, b) => a * b;
       setPrevious();
     }
@@ -92,33 +82,34 @@ export default {
     }
     function add() {
       operator.value = (a, b) => a + b;
-
       setPrevious();
     }
     function equal() {
-      current.value = operator.value(previous.value, current.value);
-      buttonDisable.value = false;
+      current.value = operator.value(
+        parseFloat(previous.value),
+       parseFloat(current.value)
+      );
       console.log(current.value);
       console.log(previous.value);
-      previous.value = null;
+      previous.value=null;
     }
     return {
-      buttonDisable,
-      equal,
+      current,  
+      previous,
+      operator,
+      operatorClick,
+      clear,
+      two,
+      percent,
+      append,
+      dot,
+      del,
+      setPrevious,
       divide,
-      times,
+      multiply,
       minus,
       add,
-      dot,
-      clearAll,
-      sign,
-      percent,
-      del,
-      append,
-      current,
-      operator,
-      previous,
-      operatorClicked,
+      equal,
     };
   },
 };
@@ -183,10 +174,14 @@ export default {
   overflow: hidden;
   box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.4), inset;
 }
-
 .operator {
   background: chartreuse;
   color: #2c3e50;
+  border-radius: 50px;
+  margin: 5px;
+  padding: 20px 5px 20px 5px;
+  margin-top: 20px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.4);
 }
 .operator:hover {
   background: rgb(92, 184, 0);
