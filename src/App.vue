a10d65139d456e5e170520fd685c8deb7a68194d
naraja03 <54923055+naraja03@users.noreply.github.com>
2021-12-27 17:47:52 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,23 @@
     <h1>Calculator!</h1>
     <div class="calculator">
       <div class="display">{{ current || "0" }}</div>
-      <div @click="clear" class="btn">AC</div>
-      <div @click="two" class="btn">+/-</div>
-      <div @click="percent" class="btn">%</div>
+      <div @click="clear" class="button">AC</div>
+      <div @click="two" class="button">+/-</div>
+      <div @click="percent" class="button">%</div>
       <div @click="divide" class="operator">/</div>
-      <div @click="append('7')" class="btn">7</div>
-      <div @click="append('8')" class="btn">8</div>
-      <div @click="append('9')" class="btn">9</div>
+      <div @click="append('7')" class="button">7</div>
+      <div @click="append('8')" class="button">8</div>
+      <div @click="append('9')" class="button">9</div>
       <div @click="multiply" class="operator">*</div>
-      <div @click="append('4')" class="btn">4</div>
-      <div @click="append('5')" class="btn">5</div>
-      <div @click="append('6')" class="btn">6</div>
+      <div @click="append('4')" class="button">4</div>
+      <div @click="append('5')" class="button">5</div>
+      <div @click="append('6')" class="button">6</div>
       <div @click="minus" class="operator">-</div>
-      <div @click="append('1')" class="btn">1</div>
-      <div @click="append('2')" class="btn">2</div>
-      <div @click="append('3')" class="btn">3</div>
+      <div @click="append('1')" class="button">1</div>
+      <div @click="append('2')" class="button">2</div>
+      <div @click="append('3')" class="button">3</div>
       <div @click="add" class="operator">+</div>
-      <div @click="append(0)" class="btn zero">0</div>
+      <div @click="append(0)" class="button">0</div>
       <div @click="del" class="operator">DEL</div>
 
       <div @click="dot" class="button">.</div>
